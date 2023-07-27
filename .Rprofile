8050d27a96740c5b31bce1a6c409184068671613
PatrickCoyle <patrick.coyle@temple.edu>
2023-07-26 21:15:31 -0500

    Added second step to customize input formats for new target.
    Signed-off-by: PatrickCoyle <patrick.coyle@temple.edu>

--- .Rprofile
+++ .Rprofile
@@ -1,1 +1,12 @@
 source("renv/activate.R")
+pacman::p_load(
+  dplyr,
+  devtools,
+  here,
+  miniUI,
+  shiny,
+  sortable,
+  targets,
+  testthat,
+  usethis
+)
--- R/targetsAddin.R
+++ R/targetsAddin.R
@@ -1,54 +1,90 @@
 #' Add-in for building a new target in a pipeline
 #'
+#' @importFrom rlang .data
 #' @param script Character of length 1, path to the target script file. See \link[targets]{tar_manifest} for details.
 #' @return a list of file names and file contents. the app has a side effect of creating file with the specified names and contents.
 #' @export
 targetsAddin <- function (
     script = targets::tar_config_get("script")
 ) {
-
   input_targets <- as.list(targets::tar_manifest(script = script)$name)
-
   ui <- miniUI::miniPage(
     miniUI::gadgetTitleBar("Build New Target"),
-    miniUI::miniContentPanel(
-      shiny::textInput("new_target", "New Target Name"),
-      shiny::textInput("new_function", "New Function Name"),
-      sortable::bucket_list(
-        header = "Drag and drop inputs for new function",
-        group_name = "bucket_list_groups",
-        orientation = "horizontal",
-        sortable::add_rank_list(
-          text = "Available Inputs",
-          labels = input_targets,
-          input_id = "exclude"
-        ),
-        sortable::add_rank_list(
-          text = "Selected Inputs",
-          labels = NULL,
-          input_id = "include"
+    miniUI::miniTabstripPanel(
+      miniUI::miniTabPanel(
+        "Step 1",
+        miniUI::miniContentPanel(
+          miniUI::miniButtonBlock(
+            shiny::textInput("new_target", "New Target Name"),
+            shiny::textInput("new_function", "New Function Name")
+          ),
+          sortable::bucket_list(
+            header = "Drag and drop inputs for new function",
+            group_name = "bucket_list_groups",
+            orientation = "horizontal",
+            sortable::add_rank_list(
+              text = "Available Inputs",
+              labels = input_targets,
+              input_id = "exclude"
+            ),
+            sortable::add_rank_list(
+              text = "Selected Inputs",
+              labels = NULL,
+              input_id = "include"
+            )
+          )
+        )
+      ),
+      miniUI::miniTabPanel(
+        "Step 2",
+        miniUI::miniContentPanel(
+          rhandsontable::rHandsontableOutput("tbl1")
         )
       )
+
     )
   )
-
   server <- function(input, output, session) {
+    rv1 <- shiny::reactiveValues(dat = NULL)
+    shiny::observeEvent(input$bucket_list_groups$include, {
+      rv1$dat <- tibble::tibble(
+        Target = unlist(input$bucket_list_groups$include)
+      ) %>%
+        dplyr::mutate(
+          Input_Name = .data[["Target"]],
+          Import_Option = factor("as is",
+                                 levels = c("as is",
+                                            "readr::read_rds",
+                                            "readr::read_csv",
+                                            "openxlsx2::read_xlsx",
+                                            "readr::read_delim(delim = '|')",
+                                            "haven::read_sas"),
+                                 ordered = TRUE
+          )
+        )
+    })
+    output$tbl1 <- rhandsontable::renderRHandsontable({
+      rhandsontable::rhandsontable(rv1$dat, height = 300)
+    })
     shiny::observeEvent(input$done, {
-      my_output_names <- c("bucket_list_groups", "new_target", "new_function") %>%
-        purrr::set_names()
-      my_output <- my_output_names %>%
-        purrr::map(~input[[.x]])
-      formatted <- helper1_20230619(my_output)
-      helper_fn_dir <- dirname(formatted$filename_fn)
+      tmp1 <- rhandsontable::hot_to_r(input$tbl1)
+      tmp2 <- c(
+        "new_target",
+        "new_function"
+      ) %>%
+        purrr::set_names() %>%
+        purrr::map(~input[[.x]]) %>%
+        c(list("new_inputs_df" = tmp1))
+      tmp3 <- helper2_20230726(tmp2)
+      helper_fn_dir <- dirname(tmp3$filename_fn)
       if (!dir.exists(helper_fn_dir)) {
         dir.create(helper_fn_dir)
       }
-      readr::write_lines(formatted$txt_fn, formatted$filename_fn)
-      readr::write_lines(formatted$txt_tgt, formatted$filename_tgt)
-      utils::file.edit(formatted$filename_tgt, formatted$filename_fn)
-      shiny::stopApp(my_output)
+      readr::write_lines(tmp3$txt_fn, tmp3$filename_fn)
+      readr::write_lines(tmp3$txt_tgt, tmp3$filename_tgt)
+      utils::file.edit(tmp3$filename_tgt, tmp3$filename_fn)
+      shiny::stopApp(tmp3)
     })
   }
-
   shiny::runGadget(ui, server)
 }
