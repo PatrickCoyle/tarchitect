3353c508df5f29f4a7ebb46f4278d48081cbeff6
PatrickCoyle <patrick.coyle@temple.edu>
2023-07-28 19:12:58 -0500

    Made quarto extension import do less.
    Signed-off-by: PatrickCoyle <patrick.coyle@temple.edu>

--- R/use_quarto_ext.R
+++ R/use_quarto_ext.R
@@ -1,25 +1,16 @@
 #' Install bundled Quarto extensions into current working directory
 #'
-#' @param file_name A valid file name
 #' @param ext_name String indicating which extension to install
 #'
 #' @return a message if extension was successfully copied over
 #' @export
-use_quarto_ext <- function(file_name = NULL,
-                           ext_name = "document-fn") {
+use_quarto_ext <- function(ext_name = "document-fn") {
 
-  if (is.null(file_name)) {
-    stop("You must provide a valid file_name")
-  }
-
-  out_dir <- file_name
-
-  if(!dir.exists(out_dir)) {
-    dir.create(out_dir)
-  }
-
-  # check for available extensions
-  all_available_extensions <- c("document-fn")
+  all_available_extensions <- list.dirs(
+    system.file("extdata/_extensions/", package = "targetthis"),
+    recursive = FALSE,
+    full.names = FALSE
+  )
   stopifnot("Extension not in package" = ext_name %in% all_available_extensions)
 
   # check for existing _extensions directory
@@ -29,13 +20,11 @@ use_quarto_ext <- function(file_name = NULL,
   # various reading of key-value pairs for reporting
   ext_yml <- readLines(system.file("extdata/_extensions/document-fn/_extension.yml",
                                    package = "targetthis"))
-
   ext_ver <- gsub(
     x = ext_yml[grepl(x = ext_yml, pattern = "version:")],
     pattern = "version: ",
     replacement = ""
   )
-
   ext_nm <- gsub(
     x = ext_yml[grepl(x = ext_yml, pattern = "title:")],
     pattern = "title: ",
@@ -62,12 +51,4 @@ use_quarto_ext <- function(file_name = NULL,
   } else {
     message("Extension appears not to have been created")
   }
-
-  # create new qmd report based on template
-  readLines("_extensions/document-fn/template.qmd") |>
-    writeLines(text = _,
-               con = paste0(out_dir, "/", file_name, ".qmd", collapse = ""))
-
-  # open the new file in the editor
-  file.edit(paste0(out_dir, "/", file_name, ".qmd", collapse = ""))
 }
