227433b149d262f73c30765482c5f88c444877b7
PatrickCoyle <patricktmc@gmail.com>
2024-08-11 00:47:38 -0500

    Removing RStudio addins

--- R/targets_addin2.R
+++
@@ -1,9 +0,0 @@
-#' Add-in for building a new target in a pipeline
-#'
-#' @export
-targets_addin2 <- function() {
-  targets::tar_load(
-    rstudioapi::getActiveDocumentContext()$selection[[1]]$text,
-    envir = .GlobalEnv
-  )
-}
